d968e90b96157701909ed8a0dfb8136aeddbc73a
VidoLinkBot <behruzziyoqulov7@gmail.com>
2025-08-16 09:16:41 +0500

    Update Users.py

--- Users.py
+++ Users.py
@@ -1,27 +1,8 @@
-# Foydalanuvchi ma’lumotlarini boshqarish
-
+# User ma'lumotlarini vaqtincha xotirada saqlash
 users = {}
 
-def add_user(user_id, name):
-    if user_id not in users:
-        users[user_id] = {
-            "name": name,
-            "language": None,
-            "vip": False,
-            "referral": None
-        }
-
-def set_language(user_id, lang_code):
-    if user_id in users:
-        users[user_id]["language"] = lang_code
-
-def set_vip(user_id, status=True):
-    if user_id in users:
-        users[user_id]["vip"] = status
-
-def set_referral(user_id, referral_code):
-    if user_id in users:
-        users[user_id]["referral"] = referral_code
+def add_user(user_id, data):
+    users[user_id] = data
 
 def get_user(user_id):
     return users.get(user_id, None)
